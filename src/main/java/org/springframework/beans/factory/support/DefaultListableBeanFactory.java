dbbd680fb2c41650c4db0eb9bce078884ec3c575
jieefeng <1627017086@qq.com>
2024-12-07 20:55:47 +0800

    读取xml文件

--- src/main/java/org/springframework/beans/factory/support/DefaultListableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -8,10 +8,6 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @author derekyi
- * @date 2020/11/22
- */
 public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry {
 
 	private Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
@@ -22,12 +18,17 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
 	}
 
 	@Override
-	protected BeanDefinition getBeanDefinition(String beanName) throws BeansException {
+	public BeanDefinition getBeanDefinition(String beanName) throws BeansException {
 		BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
 		if (beanDefinition == null) {
 			throw new BeansException("No bean named '" + beanName + "' is defined");
 		}
 
 		return beanDefinition;
+	}
+
+	@Override
+	public boolean containsBeanDefinition(String beanName) {
+		return beanDefinitionMap.containsKey(beanName);
 	}
 }
