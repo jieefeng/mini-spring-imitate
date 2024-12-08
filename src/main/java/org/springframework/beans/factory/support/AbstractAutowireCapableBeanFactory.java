7ded0831eb51b5991e8ab07bedefdbf184e54875
jieefeng <1627017086@qq.com>
2024-12-08 15:17:37 +0800

    容器扩展机制

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -3,12 +3,14 @@ package org.springframework.beans.factory.support;
 import cn.hutool.core.bean.BeanUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Method;
 
-public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory implements AutowireCapableBeanFactory {
 
 	private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
 
@@ -23,6 +25,9 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 			bean = createBeanInstance(beanDefinition);
 			//为bean填充属性
 			applyPropertyValues(beanName, bean, beanDefinition);
+
+			//执行bean的初始化方法和BeanPostProcessor的前置和后置处理方法
+			initializeBean(beanName, bean, beanDefinition);
 		} catch (Exception e) {
 			throw new BeansException("Instantiation of bean failed", e);
 		}
@@ -70,4 +75,59 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 	public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
 		this.instantiationStrategy = instantiationStrategy;
 	}
+
+	protected Object initializeBean(String beanName, Object bean, BeanDefinition beanDefinition) {
+		//执行BeanPostProcessor的前置处理
+		Object wrappedBean = applyBeanPostProcessorsBeforeInitialization(bean, beanName);
+
+		//TODO 后面会在此处执行bean的初始化方法
+		invokeInitMethods(beanName, wrappedBean, beanDefinition);
+
+		//执行BeanPostProcessor的后置处理
+		wrappedBean = applyBeanPostProcessorsAfterInitialization(bean, beanName);
+		return wrappedBean;
+	}
+
+	@Override
+	public Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName)
+			throws BeansException {
+		Object result = existingBean;
+		for (BeanPostProcessor processor : getBeanPostProcessors()) {
+			Object current = processor.postProcessBeforeInitialization(result, beanName);
+			if (current == null) {
+				return result;
+			}
+			result = current;
+		}
+		return result;
+	}
+
+	@Override
+	public Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName)
+			throws BeansException {
+
+		Object result = existingBean;
+		for (BeanPostProcessor processor : getBeanPostProcessors()) {
+			Object current = processor.postProcessAfterInitialization(result, beanName);
+			if (current == null) {
+				return result;
+			}
+			result = current;
+		}
+		return result;
+	}
+
+	/**
+	 * 执行bean的初始化方法
+	 *
+	 * @param beanName
+	 * @param bean
+	 * @param beanDefinition
+	 * @throws Throwable
+	 */
+	protected void invokeInitMethods(String beanName, Object bean, BeanDefinition beanDefinition) {
+		//TODO 后面会实现
+		System.out.println("执行bean[" + beanName + "]的初始化方法");
+	}
+
 }
--- src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -4,13 +4,19 @@ package org.springframework.beans.factory.support;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author derekyi
  * @date 2020/11/22
  */
-public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory , ConfigurableBeanFactory {
 
+	private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<>();
 	@Override
 	public Object getBean(String name) throws BeansException {
 		Object bean = getSingleton(name);
@@ -20,6 +26,16 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
 
 		BeanDefinition beanDefinition = getBeanDefinition(name);
 		return createBean(name, beanDefinition);
+	}
+
+	public void addBeanPostProcessor(BeanPostProcessor beanPostProcessor) {
+		//有则覆盖
+		this.beanPostProcessors.remove(beanPostProcessor);
+		this.beanPostProcessors.add(beanPostProcessor);
+	}
+
+	public List<BeanPostProcessor> getBeanPostProcessors() {
+		return this.beanPostProcessors;
 	}
 
 	protected abstract Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException;
